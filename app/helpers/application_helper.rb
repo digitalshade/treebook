1fbe44d7a30a9a7350b368734b540f95aa53c420
digitalshade <digitalshadeuk@gmail.com>
2014-02-15 12:42:34 +0000

    styled flass messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ end
 def resource_class
   devise_mapping.to
 end
+    
+def flash_class(type)
+   case type
+       when :alert
+            "alert-error"
+       when :notice
+            "alert-success"
+        else
+            ""
+       end
+end
+    
 end
