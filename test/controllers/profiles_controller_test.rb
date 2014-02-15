2b06a38777cb7dab0dc3c4ec7ae1afdd377717b8
digitalshade <digitalshadeuk@gmail.com>
2014-02-15 18:50:35 +0000

    updated profile page

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:tzevai).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
+    test "should return 404 on profile not found" do
+        get :show, id: "doesn't exist"
+        assert_response :not_found
+    end
+    
+    test "that variables are assigned on successful profile viewing" do
+        get :show, id: users(:tzevai).profile_name
+        assert assigns(:user)
+        assert_not_empty assigns(:statuses)
+    end
+    
+    test "that only shows the correct users statuses" do
+        get :show, id: users(:tzevai).profile_name
+        assigns(:statuses).each do |status|
+        assert_equal users(:tzevai), status.user
+            end
+    end
 end
